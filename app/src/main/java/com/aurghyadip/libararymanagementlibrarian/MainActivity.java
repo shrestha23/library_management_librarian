6e921966eaa164a20896fce726b578fa99c18892
Aurghyadip <adkundu@gmail.com>
2018-02-21 15:48:55 +0530

    added formatting to textview in book details layout

--- app/src/main/java/com/aurghyadip/libararymanagementlibrarian/MainActivity.java
+++ app/src/main/java/com/aurghyadip/libararymanagementlibrarian/MainActivity.java
@@ -1,11 +1,8 @@
 package com.aurghyadip.libararymanagementlibrarian;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -15,7 +12,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-
 import com.google.firebase.crash.FirebaseCrash;
 
 public class MainActivity extends AppCompatActivity
