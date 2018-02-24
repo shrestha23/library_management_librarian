beaddcb70829c6b7dbb38c969410e514d33f33ba
Aurghyadip Kundu <adkundu@gmail.com>
2018-02-24 20:34:48 +0530

    Adde barcode scanner
    Added barcode scanner via JourneyApps ZXing embedded

--- app/src/main/java/com/aurghyadip/librarymanagementlibrarian/ScanFragment.java
+++ app/src/main/java/com/aurghyadip/librarymanagementlibrarian/ScanFragment.java
@@ -9,15 +9,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.basgeekball.awesomevalidation.AwesomeValidation;
 import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 //TODO: Add data handling and Barcode Scanner
 
 public class ScanFragment extends Fragment {
 
     Button scanBooks;
+    Button scanIsbn;
     TextInputEditText isbn;
 
     AwesomeValidation awesomeValidation;
@@ -28,12 +32,19 @@ public class ScanFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_scan, container, false);
         scanBooks = rootView.findViewById(R.id.search_books);
+        scanIsbn = rootView.findViewById(R.id.barcode_scan);
         isbn = rootView.findViewById(R.id.book_search_field);
 
         //Awesome validation block
         awesomeValidation = new AwesomeValidation(ValidationStyle.COLORATION);
         awesomeValidation.addValidation(isbn, "^(97(8|9))?\\d{9}(\\d|X)$", getString(R.string.isbn_error));
 
+        scanIsbn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scanFromFragment();
+            }
+        });
 
         return rootView;
     }
@@ -52,5 +63,27 @@ public class ScanFragment extends Fragment {
                 }
             }
         });
+    }
+
+    private void scanFromFragment() {
+        IntentIntegrator integrator = IntentIntegrator.forSupportFragment(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.PRODUCT_CODE_TYPES);
+        integrator.setBeepEnabled(true);
+        integrator.setOrientationLocked(false);
+        integrator.initiateScan();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(getActivity(), "No ISBN Found", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                isbn.setText(result.getContents());
+            }
+        }
     }
 }
